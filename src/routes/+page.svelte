427e8f6572a5bf67a64fad065b89c326334fa31b
Maxi Meier <maximilian.meier@tum.de>
2023-12-23 19:25:11 +0100

    add custom reordering of locks (very subjective)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,34 @@
 <script lang="ts">
-	import type { Locks } from '$lib/Locks';
+	import type { Locks, Lock } from '$lib/Locks';
 	import LockComponent from '$lib/components/LockComponent.svelte';
 	import Settings from '$lib/components/Settings.svelte';
 
-	let locks: Locks;
+	let locks: Lock[];
 	/** @type {import('./$types').PageData} */
 	export let data;
-	locks = data;
+	let reorder: { [id: number]: number } = {
+		10: 1,
+		3: 2,
+		8: 3,
+		4: 4,
+		6: 5,
+		5: 6,
+		2: 7
+	};
+	locks = data.locks.sort((i1: Lock, i2: Lock) => reorder[i1.id] - reorder[i2.id]);
 </script>
 
 <main class="container" style="padding-top:0">
-	{#if locks.locks}
+	{#if locks}
 		<article style="margin-top:0">
 			<hgroup>
 				<h1>Key4Web</h1>
 				<h2>Locks</h2>
 			</hgroup>
-			{#each locks.locks as lock}
+			{#each locks as lock}
 				<LockComponent
 					id={lock.id}
-					name="{lock.name},"
+					name="{lock.name}"
 					timeout={lock.components[0].settings.timeout ?? 0}
 				></LockComponent>
 			{/each}
