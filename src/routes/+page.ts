74d0f2be65d3c36e8096a8308d1027acd54872da
Maxi Meier <maximilian.meier@tum.de>
2023-12-26 12:11:08 +0000

    fix formatting

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,6 +1,6 @@
 import type { Locks } from "$lib/Locks";
 export const load = async ({ fetch }) => {
-    const locks: Locks | undefined = await fetch('/api/lock-list', {method: "POST"})
+    const locks: Locks | undefined = await fetch('/api/lock-list', { method: "POST" })
         .then((list) => list.json())
         .catch(() => undefined)
     //.then((list) => (locks = list));
